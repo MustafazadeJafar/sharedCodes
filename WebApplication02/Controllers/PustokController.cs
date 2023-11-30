3ab9c5c6d71468cf4fd986b505b9c6d88cee19e2
master <mi625nkm8@code.edu.az>
2023-11-30 13:34:51 +0400

    web02 pastak

---
+++ WebApplication02/Controllers/PustokController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication02.Controllers;
+
+public class PustokController : Controller
+{
+    public IActionResult Index()
+    {
+
+        return View();
+    }
+}
